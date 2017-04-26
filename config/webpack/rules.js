d23b2566b745489e51713c8e6b132773bba8f95b
Kevin Altman <kevin@itsthatguy.com>
2017-04-26 17:58:22 -0400

    adds eslint rule

--- config/webpack/rules.js
+++ config/webpack/rules.js
@@ -1,3 +1,21 @@
+const paths = require('../paths');
+
 module.exports = [
-  { parser: { requireEnsure: false } }
+  { parser: { requireEnsure: false } },
+  {
+    test: /\.jsx?$/,
+    enforce: 'pre',
+    include: paths.appSrc,
+    use: [
+      {
+        loader: 'eslint-loader',
+        options: {
+          baseConfig: {
+            extends: ['adorable'],
+          },
+          useEslintrc: false,
+        }
+      }
+    ]
+  }
 ];
