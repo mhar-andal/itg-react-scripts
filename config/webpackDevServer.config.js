4e7d5232da2399b131e3138dc812e77fba0ba2f4
Zachery Moneypenny <whazzmaster@gmail.com>
2017-05-10 11:17:07 -0500

    Fixes HOT, HOT, HOT reloading

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -38,7 +38,7 @@ module.exports = {
   hot: true,
   // It is important to tell WebpackDevServer to use the same "root" path
   // as we specified in the config. In development, we always serve from /.
-  publicPath: config.output.publicPath,
+  publicPath: '/',
   // WebpackDevServer is noisy by default so we emit custom message instead
   // by listening to the compiler events with `compiler.plugin` calls above.
   quiet: true,
